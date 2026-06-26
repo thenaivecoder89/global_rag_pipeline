099fdc7ec7590238c4f36587fabee58e30f44c74
thenaivecoder89 <anuragsarangi13@gmail.com>
2026-06-26 13:46:49 +0400

    Updated API program

--- global_rag/scripts/main.py
+++ global_rag/scripts/main.py
@@ -11,7 +11,7 @@ origins = config_base["allowed_origins"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"]
 )
