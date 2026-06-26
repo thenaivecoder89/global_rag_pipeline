a4da4ec77cb0b7e99a6ab2ad76505ac979aa67b1
thenaivecoder89 <anuragsarangi13@gmail.com>
2026-06-26 12:31:04 +0400

    API Program

--- global_rag/scripts/main.py
+++ global_rag/scripts/main.py
@@ -2,11 +2,27 @@ import build_document_inventory
 import config
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse
 
 app = FastAPI()
 
-config_settings = config.config(project_root="/home/anurag_sarangi/projects/global_rag_pipeline/global_rag/")
+config_base = config.config_base()
+origins = config_base["allowed_origins"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
-origins = config.config(project_root="/home/anurag_sarangi/projects/global_rag_pipeline/global_rag/").allowed_origins
-
-print(origins)
+@app.get(path="/build_document_inventory", status_code=200)
+def build_doc_inv(payload: str):
+    build_document_inventory_output = build_document_inventory(payload)
+    api_response = JSONResponse(
+        {
+            "status": "ok",
+            "output": build_document_inventory_output
+        }
+    )
+    return api_response
